7a3808ab948c1a34ccd2b789054bf175281329ba
rajathshttgr <rajathshettigar.uy@gmail.com>
2025-10-24 17:40:58 +0530

    add test case

--- test/test.js
+++ test/test.js
@@ -1,3 +1,16 @@
 import qwiker from "../index.js";
 
 console.log("Qwiker Version:", qwiker.VERSION);
+
+let api_response = "";
+
+const api = qwiker.create({
+  baseURL: `https://jsonplaceholder.typicode.com`,
+  headers: { "Content-Type": "application/json" },
+});
+
+api
+  .get(`/posts/1`)
+  .then((res) => (api_response = res))
+  .catch((err) => (api_response = err.message))
+  .finally(() => console.log("Response Message:", api_response));
